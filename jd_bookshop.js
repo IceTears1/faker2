bada4cb0e106e725d567aeb373c05fd156e91c50
FY <zhangfeiyue2786@yundasys.com>
2021-08-31 19:43:19 +0800

    no message

--- jd_bookshop.js
+++ jd_bookshop.js
@@ -690,7 +690,7 @@ function TotalBean() {
 //格式化助力码
 function shareCodesFormat() {
   return new Promise(async resolve => {
-    // console.log(`第${$.index}个京东账号的助力码:::${$.shareCodesArr[$.index - 1]}`)
+    console.log(`第${$.index}个京东账号的助力码:::${$.shareCodesArr[$.index - 1]}`)
     $.newShareCodes = [];
     if ($.shareCodesArr[$.index - 1]) {
       $.newShareCodes = $.shareCodesArr[$.index - 1].split('@');
