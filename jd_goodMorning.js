7edee52243b7daf28f9988d8fc5f597a336877aa
FY <zhangfeiyue2786@yundasys.com>
2021-09-01 14:14:59 +0800

    no message

--- jd_goodMorning.js
+++ jd_goodMorning.js
@@ -137,7 +137,7 @@ function TotalBean() {
 
 function randomString(e) {
     e = e || 32;
-    let t = "abcdefhijkmnprstwxyz2345678",
+    let t = "zbcdefhijkmnprstwxyz3456789",
         a = t.length,
         n = "";
     for (i = 0; i < e; i++)
