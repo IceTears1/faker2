afc3a275fcf56b04f2110cbd702887ceae940c1e
zhfy65396567 <642203775@qq.com>
2021-09-21 12:08:51 +0800

    asdas

--- jd_jxmc.js
+++ jd_jxmc.js
@@ -80,7 +80,7 @@ if ($.isNode()) {
   }
   console.log('\n##################开始账号内互助#################\n');
   let newCookiesArr = [];
-  for(let i = 0; i < $.helpCkList.length; i += 4) {
+  for(let i = 1; i < $.helpCkList.length; i += 4) {
     newCookiesArr.push($.helpCkList.slice(i, i + 4))
   }
   for (let i = 0; i < newCookiesArr.length; i++) {
