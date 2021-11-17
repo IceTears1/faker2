ea8e0b61f437449a5a84bb63860082e5f6f9c0b3
zhfy65396567 <642203775@qq.com>
2021-11-17 23:47:40 +0800

    ADSD

--- jd_jxmc.js
+++ jd_jxmc.js
@@ -73,7 +73,7 @@ if ($.isNode()) {
     }
     if (process.env.JXMC_RP != 'false' && flag_hb) {
         console.log('\n##################开始账号内互助(红包)#################\n');
-        await getShareCode('jxmc_hb.json')
+    
         $.inviteCodeList_rp = [...($.inviteCodeList_rp || []), ...($.shareCode || [])]
         for (let j = 0; j < cookiesArr.length; j++) {
             $.cookie = cookiesArr[j];
@@ -97,7 +97,6 @@ if ($.isNode()) {
     }
     console.log('\n##################开始账号内互助#################\n');
     $.shareCode = undefined
-    await getShareCode('jxmc.json')
     $.inviteCodeList = [...($.inviteCodeList || []), ...($.shareCode || [])]
     for (let j = 0; j < cookiesArr.length; j++) {
         $.cookie = cookiesArr[j];
@@ -128,30 +127,7 @@ if ($.isNode()) {
         }
     }
 })().catch((e) => {$.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')}).finally(() => {$.done();})
-function getShareCode(name) {
-  return new Promise(resolve => {
-    $.get({
-      url: "https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/"+name,
-      headers: {
-        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
-      }
-    }, async (err, resp, data) => {
-      try {
-        if (err) {
-          console.log(`${JSON.stringify(err)}`);
-          console.log(`${$.name} API请求失败，请检查网路重试`);
-        } else {
-          console.log(`优先账号内部互助，有剩余助力次数再帮【zero205】助力`);
-          $.shareCode = JSON.parse(data);
-        }
-      } catch (e) {
-        $.logErr(e, resp)
-      } finally {
-        resolve();
-      }
-    })
-  })
-}
+
 async function get_rp(){
     let rpInfo = await takeRequest(`jxmc`,`operservice/GetInviteStatus`,``,``,true);
     if (rpInfo.ret === 0) {
